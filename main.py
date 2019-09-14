e55f552a0e551e91ff5344cf8f700372bda461db
timmysutanto <43670659+timmysutanto@users.noreply.github.com>
2019-09-14 16:55:30 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+def balikPiece(baris,kolom):
+    # Untuk membalik piece pada sebuah kotak
+    global board
+    if board[baris][kolom] == 'o':
+        board[baris][kolom] = 'x'
+    elif board[baris][kolom] == 'x':
+        board[baris][kolom] = 'o'
+    else:
+        # ERROR
+        print("Blok Kosong!!!")
