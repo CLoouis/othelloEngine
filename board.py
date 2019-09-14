74e9fe7b48c7cae0d96373da469ad84266ad8dba
louis <louiscahyadi23@gmail.com>
2019-09-14 20:45:06 +0700

    tambah nomor baris kolom

--- board.py
+++ board.py
@@ -2,10 +2,11 @@ board = [[' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], [' ', ' ', ' ', ' ', ' ', ' '
 
 def showBoard(board):
     """Show othello board based on board parameter"""
-    batas = '\n' + '+---'*8 + '+\n'
+    print('    0   1   2   3   4   5   6   7')
+    batas = '\n  ' + '+---'*8 + '+\n'
     ful = batas     
     for i in range(8) :
-        sebaris = "| "
+        sebaris = str(i) + " | "
         for j in range(8) :
             sebaris += board[i][j] + ' | '
         ful += sebaris + batas
@@ -15,4 +16,15 @@ board[3][3] = 'o'
 board[3][4] = 'x'
 board[4][3] = 'x'
 board[4][4] = 'o'
-showBoard(board)
+# showBoard(board)
+
+# def main():
+#     while (True):
+#         baris = int(input("Silahkan pilih baris : "))
+#         kolom = int(input("Silahkan pilih kolom : "))
+
+#         board[baris][kolom] = 'o'
+
+#         showBoard(board)
+
+# main()
