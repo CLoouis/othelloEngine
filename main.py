33701f924ff92f271de7864ef4bd8eca1529a616
timmysutanto <43670659+timmysutanto@users.noreply.github.com>
2019-09-14 23:21:47 +0700

    Added legality checking

--- main.py
+++ main.py
@@ -31,12 +31,10 @@ def isJalanLegal(giliran,baris,kolom):
 def removeKoordinatDariArrayLegal(giliran,baris,kolom):
     global arrayLegalMovesO,arrayLegalMovesX,playTurn
     tupleKoordinatJalan = (int(baris),int(kolom))
-    if(giliran == 'o'):
+    if(tupleKoordinatJalan in arrayLegalMovesO):
         arrayLegalMovesO.remove(tupleKoordinatJalan)
-    elif(giliran == 'x'):
+    if(tupleKoordinatJalan in arrayLegalMovesX):
         arrayLegalMovesX.remove(tupleKoordinatJalan)
-    else:
-        return False
 
 def jalan(posisi):
     global board,playTurn,koordinat
@@ -45,11 +43,6 @@ def jalan(posisi):
         board[int(koordinatJalan[0])][int(koordinatJalan[1])] = playTurn
         # Remove koordinatJalan dari arrayLegalMoves
         removeKoordinatDariArrayLegal(playTurn,int(koordinatJalan[0]),int(koordinatJalan[1]))
-        # Mengganti giliran jalan
-        if(playTurn == 'o'):
-            playTurn = 'x'
-        elif(playTurn =='x'):
-            playTurn = 'o'
     else:
         # Koordinat tidak legal
         print("I'm sorry, but that move is invalid my friend. Please input another one. Which grid you want to play?")
@@ -57,14 +50,222 @@ def jalan(posisi):
         jalan(koordinat)
 
 def isGridLegal(playTurn,baris,kolom):
-    if(baris == 0 or baris == 10 or kolom == 0 or baris == 10):
+    global board
+    if(baris == 0 or baris == 9 or kolom == 0 or baris == 9):
         # Di luar papan
         return False
     elif(not isGridKosong(baris,kolom)):
         # Sudah terisi
         return False
     else:
-        return True
+        if(playTurn == 'x'):
+            # Cek ke atas
+            indeksBaris = baris
+            indeksKolom = kolom
+            
+            # Cek apakah harus di cek ke atas
+            if(board[indeksBaris-1][indeksKolom] == 'x'):
+                while(indeksBaris > 0):
+                    if(board[indeksBaris-1][indeksKolom] == 'o'):
+                        # Ketemu temen
+                        return True
+                    else:
+                        indeksBaris = indeksBaris - 1
+
+            # Cek ke kanan atas
+            indeksBaris = baris
+            indeksKolom = kolom
+            
+            # Cek apakah harus di cek ke kanan atas
+            if(board[indeksBaris-1][indeksKolom+1] == 'x'):
+                while(indeksBaris > 0 and indeksKolom < 9):
+                    if(board[indeksBaris-1][indeksKolom+1] == 'o'):
+                        return True
+                    else:
+                        indeksBaris = indeksBaris - 1
+                        indeksKolom = indeksKolom + 1
+
+            # Cek ke kanan
+            indeksBaris = baris
+            indeksKolom = kolom
+            
+            # Cek apakah harus di cek ke kanan
+            if(board[indeksBaris][indeksKolom+1] == 'x'):
+                while(indeksKolom < 9):
+                    if(board[indeksBaris][indeksKolom+1] == 'o'):
+                        return True
+                    else:
+                        indeksKolom = indeksKolom + 1
+
+            # Cek ke kanan bawah
+            indeksBaris = baris
+            indeksKolom = kolom
+            
+            # Cek apakah harus di cek ke kanan bawah
+            if(board[indeksBaris+1][indeksKolom+1] == 'x'):
+                while(indeksBaris < 9 and indeksKolom < 9):
+                    if(board[indeksBaris+1][indeksKolom+1] == 'o'):
+                        return True
+                    else:
+                        indeksKolom = indeksKolom + 1
+                        indeksBaris = indeksBaris + 1
+
+            # Cek ke bawah
+            indeksBaris = baris
+            indeksKolom = kolom
+            
+            # Cek apakah harus di cek ke bawah
+            if(board[indeksBaris+1][indeksKolom] == 'x'):
+                while(indeksBaris < 9):
+                    if(board[indeksBaris+1][indeksKolom] == 'o'):
+                        return True
+                    else:
+                        indeksBaris = indeksBaris + 1
+
+            # Cek ke kiri bawah
+            indeksBaris = baris
+            indeksKolom = kolom
+            
+            # Cek apakah harus di cek ke kiri bawah
+            if(board[indeksBaris+1][indeksKolom-1] == 'x'):
+                while(indeksBaris < 9 and indeksKolom > 0):
+                    if(board[indeksBaris+1][indeksKolom-1] == 'o'):
+                        return True
+                    else:
+                        indeksBaris = indeksBaris + 1
+                        indeksKolom = indeksKolom - 1
+
+            # Cek ke kiri
+            indeksBaris = baris
+            indeksKolom = kolom
+            
+            # Cek apakah harus di cek ke kiri
+            if(board[indeksBaris][indeksKolom-1] == 'x'):
+                while(indeksKolom > 0):
+                    if(board[indeksBaris][indeksKolom-1] == 'o'):
+                        return True
+                    else:
+                        indeksKolom = indeksKolom - 1
+
+            # Cek ke kiri atas
+            indeksBaris = baris
+            indeksKolom = kolom
+            
+            # Cek apakah harus di cek ke kiri atas
+            if(board[indeksBaris-1][indeksKolom-1] == 'x'):
+                while(indeksBaris > 0 and indeksKolom > 0):
+                    if(board[indeksBaris-1][indeksKolom-1] == 'o'):
+                        return True
+                    else:
+                        indeksKolom = indeksKolom - 1
+                        indeksBaris = indeksBaris - 1
+            
+            return False
+            
+        elif(playTurn == 'o'):
+            # Cek ke atas
+            indeksBaris = baris
+            indeksKolom = kolom
+            
+            # Cek apakah harus di cek ke atas
+            if(board[indeksBaris-1][indeksKolom] == 'o'):
+                while(indeksBaris > 0):
+                    if(board[indeksBaris-1][indeksKolom] == 'x'):
+                        # Ketemu temen
+                        return True
+                    else:
+                        indeksBaris = indeksBaris - 1
+
+            # Cek ke kanan atas
+            indeksBaris = baris
+            indeksKolom = kolom
+            
+            # Cek apakah harus di cek ke kanan atas
+            if(board[indeksBaris-1][indeksKolom+1] == 'o'):
+                while(indeksBaris > 0 and indeksKolom < 9):
+                    if(board[indeksBaris-1][indeksKolom+1] == 'x'):
+                        return True
+                    else:
+                        indeksBaris = indeksBaris - 1
+                        indeksKolom = indeksKolom + 1
+
+            # Cek ke kanan
+            indeksBaris = baris
+            indeksKolom = kolom
+            
+            # Cek apakah harus di cek ke kanan
+            if(board[indeksBaris][indeksKolom+1] == 'o'):
+                while(indeksKolom < 9):
+                    if(board[indeksBaris][indeksKolom+1] == 'x'):
+                        return True
+                    else:
+                        indeksKolom = indeksKolom + 1
+
+            # Cek ke kanan bawah
+            indeksBaris = baris
+            indeksKolom = kolom
+            
+            # Cek apakah harus di cek ke kanan bawah
+            if(board[indeksBaris+1][indeksKolom+1] == 'o'):
+                while(indeksBaris < 9 and indeksKolom < 9):
+                    if(board[indeksBaris+1][indeksKolom+1] == 'x'):
+                        return True
+                    else:
+                        indeksKolom = indeksKolom + 1
+                        indeksBaris = indeksBaris + 1
+
+            # Cek ke bawah
+            indeksBaris = baris
+            indeksKolom = kolom
+            
+            # Cek apakah harus di cek ke bawah
+            if(board[indeksBaris+1][indeksKolom] == 'o'):
+                while(indeksBaris < 9):
+                    if(board[indeksBaris+1][indeksKolom] == 'x'):
+                        return True
+                    else:
+                        indeksBaris = indeksBaris + 1
+
+            # Cek ke kiri bawah
+            indeksBaris = baris
+            indeksKolom = kolom
+            
+            # Cek apakah harus di cek ke kiri bawah
+            if(board[indeksBaris+1][indeksKolom-1] == 'o'):
+                while(indeksBaris < 9 and indeksKolom > 0):
+                    if(board[indeksBaris+1][indeksKolom-1] == 'x'):
+                        return True
+                    else:
+                        indeksBaris = indeksBaris + 1
+                        indeksKolom = indeksKolom - 1
+
+            # Cek ke kiri
+            indeksBaris = baris
+            indeksKolom = kolom
+            
+            # Cek apakah harus di cek ke kiri
+            if(board[indeksBaris][indeksKolom-1] == 'o'):
+                while(indeksKolom > 0):
+                    if(board[indeksBaris][indeksKolom-1] == 'x'):
+                        return True
+                    else:
+                        indeksKolom = indeksKolom - 1
+
+            # Cek ke kiri atas
+            indeksBaris = baris
+            indeksKolom = kolom
+            
+            # Cek apakah harus di cek ke kiri atas
+            if(board[indeksBaris-1][indeksKolom-1] == 'o'):
+                while(indeksBaris > 0 and indeksKolom > 0):
+                    if(board[indeksBaris-1][indeksKolom-1] == 'x'):
+                        return True
+                    else:
+                        indeksKolom = indeksKolom - 1
+                        indeksBaris = indeksBaris - 1
+            
+            return False
+
 
 def addArrayLegal(playTurn,baris,kolom):
     global arrayLegalMovesO,arrayLegalMovesX
@@ -103,6 +304,15 @@ def updateArrayLegalMove(playTurn,koordinat):
     if isGridLegal(playTurn,int(koordinatJalan[0])+1, int(koordinatJalan[1])+1):
         addArrayLegal(playTurn, int(koordinatJalan[0])+1, int(koordinatJalan[1])+1)
 
+def switchPlay():
+    global playTurn
+    if(playTurn == 'o'):
+        playTurn = 'x'
+    elif(playTurn == 'x'):
+        playTurn = 'o'
+    else:
+        print('ERROR')
+
     
 
 arrayLegalMovesO = [(5,3),(6,4),(3,5),(4,6)]
@@ -120,6 +330,7 @@ while not gameEnd:
     koordinat = input("")
     jalan(koordinat)
     updateArrayLegalMove(playTurn,koordinat)
+    switchPlay()
 
     # For checking purpose only. Delete if the project is finished.
     print('arrayLegalMovesO : ',arrayLegalMovesO)
