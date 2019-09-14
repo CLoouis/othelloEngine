3ae869f89ba67218031c05d162b67296771bc47c
timmysutanto <43670659+timmysutanto@users.noreply.github.com>
2019-09-14 17:50:27 +0700

    added move legality checking

--- main.py
+++ main.py
@@ -13,10 +13,26 @@ def balikPiece(baris,kolom):
         # ERROR
         print("Blok Kosong!!!")
 
+def isGridKosong(baris,kolom):
+    global board
+    return (board[baris][kolom] == ' ')
+
+def isJalanLegal(giliran,baris,kolom):
+    global arrayLegalMovesO,arrayLegalMovesX,playTurn
+    tupleKoordinatJalan = (int(baris),int(kolom))
+    if(giliran == 'o'):
+        return (tupleKoordinatJalan in arrayLegalMovesO)
+    elif(giliran == 'x'):
+        return (tupleKoordinatJalan in arrayLegalMovesX)
+    else:
+        return False
+
 def jalan(posisi):
     global board,playTurn
     koordinatJalan = posisi.split(",")
-    board[int(koordinatJalan[0])][int(koordinatJalan[1])] = playTurn
+    if(isJalanLegal(playTurn,koordinatJalan[0],koordinatJalan[1])):
+        board[int(koordinatJalan[0])][int(koordinatJalan[1])] = playTurn
+    # Mengganti giliran jalan
     if(playTurn == 'o'):
         playTurn = 'x'
     elif(playTurn =='x'):
@@ -24,16 +40,18 @@ def jalan(posisi):
     
 
 global arrayLegalMovesO, arrayLegalMovesX,playTurn
-arrayLegalMovesO = []
-arrayLegalMovesX = []
+arrayLegalMovesO = [(4,2),(5,3),(2,4),(3,5)]
+arrayLegalMovesX = [(3,2),(2,3),(5,4),(4,5)]
 
 gameEnd = 0
 playTurn = 'o'
 
 # MAIN
+
 print("GAME BEGINS!")
 while not gameEnd:
     showBoard(board)
     print("It's your turn player "+playTurn+", which grid you want to play?")
-    command = input("")
-    jalan(command)
+    koordinat = input("")
+    jalan(koordinat)
+    
