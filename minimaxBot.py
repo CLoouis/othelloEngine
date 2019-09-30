bdb37686150a5b60599e68018df91f51643d3e62
Kevin Nathaniel <kevinnathaniel497@yahoo.com>
2019-10-01 00:34:57 +0700

    Working tree

--- minimaxBot.py
+++ minimaxBot.py
@@ -4,7 +4,6 @@ from anytree import Node, RenderTree
 import copy
 
 def makeTree(board, arrayLegalMovesO, arrayLegalMovesX, playTurn, depth, parentNode):
-    initialBoard = board
     if(depth == 0):
         print("sampai di daun")
         # return
@@ -14,17 +13,20 @@ def makeTree(board, arrayLegalMovesO, arrayLegalMovesX, playTurn, depth, parentN
         else:
             array = arrayLegalMovesO
         for i in array:
+            initialBoard = copy.deepcopy(board)
+            parent = copy.deepcopy(parentNode)
             print(depth)
             stringJalan = str(i[0])+","+str(i[1])
             print(stringJalan)
-            copyBoard = jalan(initialBoard, stringJalan, playTurn, arrayLegalMovesO, arrayLegalMovesX)
+            copyBoard = copy.deepcopy(jalan(initialBoard, stringJalan, playTurn, copy.deepcopy(arrayLegalMovesO), copy.deepcopy(arrayLegalMovesX)))
             if(playTurn == 'x'):
                 playTurnCheck = 'o'
             elif(playTurn == 'o'):
                 playTurnCheck = 'x'
-            tupleArrayLegal = updateArrayLegalMove (copyBoard, arrayLegalMovesO, arrayLegalMovesX)
-            childNode = Node((copyBoard, tupleArrayLegal[0], tupleArrayLegal[1]), parent = parentNode)
-            makeTree(copy.deepcopy(copyBoard), tupleArrayLegal[0], tupleArrayLegal[1], playTurnCheck, depth-1, childNode)
+            tupleArrayLegal = updateArrayLegalMove(copyBoard, copy.deepcopy(arrayLegalMovesO), copy.deepcopy(arrayLegalMovesX))
+            childNode = Node((copyBoard, tupleArrayLegal[0], tupleArrayLegal[1]), parent)
+            
+            makeTree(copyBoard, tupleArrayLegal[0], tupleArrayLegal[1], playTurnCheck, depth-1, childNode)
             
 
 # def minimaxBot(board, arrayLegalMovesO, arrayLegalMovesX, playTurn, depth):
@@ -40,7 +42,7 @@ board[5][5] = 'o'
 arrayLegalMovesO = [(5,3),(6,4),(3,5),(4,6)]
 arrayLegalMovesX = [(4,3),(3,4),(6,5),(5,6)]
 playTurn = 'o'
-depth = 5
+depth = 2
 parentNode = Node((board, arrayLegalMovesO, arrayLegalMovesX))
 # print(RenderTree(parentNode))
 makeTree(board, arrayLegalMovesO, arrayLegalMovesX, playTurn, depth, parentNode)
