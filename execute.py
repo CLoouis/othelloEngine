b2d80c2e05099d3434b65988c3b87f456b250db7
Kevin Nathaniel <kevinnathaniel497@yahoo.com>
2019-09-30 23:19:41 +0700

    Check validity input

--- execute.py
+++ execute.py
@@ -1,4 +1,6 @@
 import randombot
+import re
+import legal
 # import minimaxBot
 
 def execute(whoami, board, arrayLegalMovesO, arrayLegalMovesX, playTurn, depth):
@@ -6,8 +8,24 @@ def execute(whoami, board, arrayLegalMovesO, arrayLegalMovesX, playTurn, depth):
         # player
         print("It's your turn player "+playTurn +
               ", which grid you want to play?")
+        print("Format: [# baris],[# kolom]")
         koordinat = input("")
-        return koordinat
+        pattern = re.compile("^[0-9],[0-9]$")
+        
+        while True:
+            if not(pattern.match(koordinat)):
+                print("Format: [# baris],[# kolom]")
+                koordinat = input("")
+            else:
+                koordinatJalan = koordinat.split(",")
+                if legal.isJalanLegal(playTurn, koordinatJalan[0], koordinatJalan[1], arrayLegalMovesO, arrayLegalMovesX):
+                    return koordinat
+                else:
+                    print("Input Tidak Valid")
+                    koordinat = "2"
+        
+        
+
     elif (whoami == "2"):
         # random bot
         tupleMove = randombot.randomMove(arrayLegalMovesO, arrayLegalMovesX, playTurn)
